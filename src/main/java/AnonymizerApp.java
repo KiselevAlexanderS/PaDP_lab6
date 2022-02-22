aaa81e0709b598280539b29ed94d457fa02aa306
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 04:10:06 +0300

    Scripted auto-commit on change (2022-02-22 04:10:06) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -1,19 +1,6 @@
-import akka.NotUsed;
-import akka.actor.ActorSystem;
-import akka.http.javadsl.ConnectHttp;
-import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
-import org.asynchttpclient.AsyncHttpClient;
-
-import java.io.IOException;
-import java.util.concurrent.CompletionStage;
 
 public class AnonymizerApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
     }
 }
