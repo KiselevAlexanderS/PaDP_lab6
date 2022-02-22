6dd6ce7ae1e04e274beea5667f38bc8832ea38ba
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 04:43:51 +0300

    Scripted auto-commit on change (2022-02-22 04:43:51) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -1,11 +1,15 @@
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StorageActor extends AbstractActor {
 
     public List<String> storage;
 
+    public StorageActor() {
+        this.storage = new ArrayList<>();
+    }
 
     @Override
     public Receive createReceive() {
