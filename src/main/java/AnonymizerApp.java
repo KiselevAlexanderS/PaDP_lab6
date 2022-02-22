977adaa4765439fccd5056b6a2836eceb812533c
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 04:18:50 +0300

    Scripted auto-commit on change (2022-02-22 04:18:50) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -3,6 +3,7 @@ public class AnonymizerApp {
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: AnonymizerApp <host> <post>");
+            System.exit(-1);
         }
     }
 }
