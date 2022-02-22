3095dd662091df784ce9b006ca996c8eb4cca5ec
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 03:30:09 +0300

    Scripted auto-commit on change (2022-02-22 03:30:09) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -2,10 +2,11 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 
-public class AnonimizerApp {
+public class AnonymizerApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materia = ActorMaterializer.create(system);
+        MainHttp instance =
     }
 }
