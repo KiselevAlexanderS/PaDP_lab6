51b07e7903b1801fd38b88119c4798f688006b59
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 04:07:03 +0300

    Scripted auto-commit on change (2022-02-22 04:07:03) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -14,20 +14,6 @@ import java.util.concurrent.CompletionStage;
 
 public class AnonymizerApp {
     public static void main(String[] args) throws IOException {
-        ActorSystem system = ActorSystem.create("routes");
-        final AsyncHttpClient asyncHttpClient = asyncHttpClient();
-        final Http http = Http.get(system);
-        final ActorMaterializer materia = ActorMaterializer.create(system);
-
-        final Flow<HttpRequest , HttpResponse, NotUsed> routeFlow = test.createFlow();
-
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-                routeFlow,
-                ConnectHttp.toHost("localhost", 8000),
-                materia
-        );
-        System.out.println("Server at localhost:8000/\n Press Enter to stop");
-        System.in.read();
 
     }
 }
