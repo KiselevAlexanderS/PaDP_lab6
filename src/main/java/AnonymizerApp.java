6f98ba5254ccb34d356e6b7e569cef9ffcd6d7fd
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 03:39:14 +0300

    Scripted auto-commit on change (2022-02-22 03:39:14) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -1,12 +1,19 @@
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import org.as
 
 public class AnonymizerApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
+        final AsyncHttpClient asyncHttpClient = a
         final ActorMaterializer materia = ActorMaterializer.create(system);
-        MainHttp instance =
+
+        final Flow<HttpRequest , HttpResponse, NotUsed> routeFlow =
     }
 }
