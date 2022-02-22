0f71277e9b657a651be41854fa6271428c3d0a20
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 04:22:47 +0300

    Scripted auto-commit on change (2022-02-22 04:22:47) by gitwatch.sh

--- src/main/java/AkkaHttpServer.java
+++ src/main/java/AkkaHttpServer.java
@@ -15,21 +15,5 @@ import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class AkkaHttpServer {
     ActorSystem system = ActorSystem.create("routes");
-    final AsyncHttpClient asyncHttpClient = asyncHttpClient();
-    final Http http = Http.get(system);
-    final ActorMaterializer materia = ActorMaterializer.create(system);
-
-    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = test.createFlow();
-
-    final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-            routeFlow,
-            ConnectHttp.toHost("localhost", 8000),
-            materia
-    );
-        System.out.println("Server at localhost:8000/\n Press Enter to stop");
-        System.in.read();
-
-        binding
-                .thenCompose(ServerBinding::unbind)
-            .thenAccept(unbound -> system.terminate())
+    
 }
