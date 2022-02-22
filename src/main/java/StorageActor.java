44e5d04915ef7f1076712dc0d9f61e3a6bc4fae0
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 04:44:11 +0300

    Scripted auto-commit on change (2022-02-22 04:44:11) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -2,10 +2,12 @@ import akka.actor.AbstractActor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class StorageActor extends AbstractActor {
 
     public List<String> storage;
+    private Random randServer;
 
     public StorageActor() {
         this.storage = new ArrayList<>();
