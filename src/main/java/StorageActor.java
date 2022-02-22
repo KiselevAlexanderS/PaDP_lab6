5215ea1e3a03bdccd27b86ed52b1be660cc3fa0e
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 04:43:16 +0300

    Scripted auto-commit on change (2022-02-22 04:43:16) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -2,9 +2,9 @@ import akka.actor.AbstractActor;
 
 public class StorageActor extends AbstractActor {
 
+    public List<String> storage;
 
 
-    
     @Override
     public Receive createReceive() {
         return null;
